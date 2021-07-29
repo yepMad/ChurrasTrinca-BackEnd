60bdc5674457554faba8c55046f4c09d3dae4bae
Eudes Tenório <eudestfilho@gmail.com>
2021-07-28 23:47:42 -0300

    Added server listener

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,18 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+
+import express from 'express';
+import cors from 'cors';
+import 'express-async-errors';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/', routes);
+
+app.listen(process.env.NODE_PORT, () => {
+  console.log(`Server started on port ${process.env.NODE_PORT}!`);
+});
