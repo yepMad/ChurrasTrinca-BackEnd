3e87e5336ef18f8be7989fdd102cc61331df8570
Eudes Tenório <eudestfilho@gmail.com>
2021-08-05 00:57:52 -0300

    Added AppError middleware

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,13 +5,17 @@ import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
+import ErrorMiddleware from '@shared/middlewares/ErrorMiddleware';
+
 import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/', routes);
+app.use(routes);
+
+app.use(ErrorMiddleware);
 
 app.listen(process.env.NODE_PORT, () => {
   console.log(`Server started on port ${process.env.NODE_PORT}!`);
---
+++ src/shared/middlewares/ErrorMiddleware.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+
+import AppError from '@shared/errors/AppError';
+
+const ErrorMiddleware = (
+  error: Error,
+  request: Request,
+  response: Response,
+  _: NextFunction,
+): Response => {
+  if (error instanceof AppError) {
+    return response
+      .status(error.statusCode)
+      .json({ status: 'error', message: error.message });
+  }
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+};
+
+export default ErrorMiddleware;
