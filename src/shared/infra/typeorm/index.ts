51aab25dbc62f0261dcf1460ee2dacd431930ec8
Eudes Tenório <eudestfilho@gmail.com>
2021-08-06 03:48:55 -0300

    Fixed: the numeric columns were taken as a string by the typeorm

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,3 +1,8 @@
 import { createConnections } from 'typeorm';
+import { types } from 'pg';
+
+types.setTypeParser(types.builtins.NUMERIC, (value: string): number =>
+  parseFloat(value),
+);
 
 createConnections();
